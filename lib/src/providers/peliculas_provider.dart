8c8286610cf83df9057ca1aa63479a2706a6090f
Bikcode <bikcodeh@gmail.com>
2020-04-28 18:14:58 -0500

    feature: comienza implementacion patron bloc

--- lib/src/providers/peliculas_provider.dart
+++ lib/src/providers/peliculas_provider.dart
@@ -1,12 +1,27 @@
-import 'package:peliculas/src/models/pelicula_model.dart';
+import 'dart:async';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:peliculas/src/models/pelicula_model.dart';
 
 class PeliculasProvider {
 
   String _apiKey = '4662e7a7fe13c9d91c80552e10a09dc1';
   String _url = 'api.themoviedb.org';
   String _language = 'es-ES';
+  int _popularesPage = 0;
+  List<Pelicula> _populares = new List();
+
+  final _popularesStreamController = StreamController<List<Pelicula>>.broadcast();
+
+  // para agregar peliculas
+  Function(List<Pelicula>) get popularesSink => _popularesStreamController.add;
+
+  //para escuchar esas peliculas
+  Stream<List<Pelicula>>get popularesStream => _popularesStreamController.stream;
+
+  void disposeStreams() {
+    _popularesStreamController?.close();
+  }
 
   Future<List<Pelicula>> _procesarRespuesta( Uri url) async {
 
@@ -27,11 +42,20 @@ class PeliculasProvider {
   }
 
   Future<List<Pelicula>> getPopulares() async {
+
+    _popularesPage++;
+
     final url = Uri.https(_url, '3/movie/popular', {
-      'api_key': _apiKey,
-      'language': _language
+      'api_key' : _apiKey,
+      'language': _language,
+      'page'    :  _popularesPage.toString()
     });
 
-    return await _procesarRespuesta(url);
+    final resp = await _procesarRespuesta(url);
+
+    _populares.addAll(resp);
+    popularesSink( _populares );
+    return resp;
+
   }
 }
