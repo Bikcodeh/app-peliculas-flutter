0a965a52e0f527cfeb7af2c3398cfad26a89c6c2
Bikcode <bikcodeh@gmail.com>
2020-04-29 22:15:26 -0500

    feature: implementa llamado a search de la api y navegacion a detalle

--- lib/src/search/search_delegate.dart
+++ lib/src/search/search_delegate.dart
@@ -1,6 +1,26 @@
 import 'package:flutter/material.dart';
 
+import 'package:peliculas/src/providers/peliculas_provider.dart';
+import 'package:peliculas/src/models/pelicula_model.dart';
+
 class DataSearch extends SearchDelegate {
+
+  final peliculas = [
+    'avengers',
+    'aquaman',
+    'batman',
+    'shazam',
+    'porky',
+    'disney',
+  ];
+
+  final peliculasRecientes = [
+    'spiderman',
+    'capitan america'
+  ];
+
+  final peliculasProvider = new PeliculasProvider();
+
   @override
   List<Widget> buildActions(BuildContext context) {
     // Son las acciones de nuestro appbar,como el de limpiar por ejemplo
@@ -31,12 +51,71 @@ class DataSearch extends SearchDelegate {
   @override
   Widget buildResults(BuildContext context) {
     // crea los resultados que se van a mostrar
-    return Container();
+    return null;
   }
-
+/* 
   @override
   Widget buildSuggestions(BuildContext context) {
     // las sugerencias que aparecen cuando la persona escribe
-    return Container();
+
+    final listaSugerida = ( query.isEmpty ) 
+                            ? peliculasRecientes
+                            : peliculas.where((p) => p.toLowerCase().startsWith(query.toLowerCase())
+                            ).toList();
+
+
+    return ListView.builder(
+      itemCount: listaSugerida.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: Icon(Icons.movie),
+          title: Text(listaSugerida[index]),
+          onTap: () {}
+        );
+      }
+    ); 
+  } */
+
+  
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // las sugerencias que aparecen cuando la persona escribe
+    if ( query.isEmpty ) {
+      return Container();
+    }
+
+    return FutureBuilder(
+      future: peliculasProvider.buscarPelicula(query),
+      builder: (BuildContext context, AsyncSnapshot<List<Pelicula>> snapshot) {
+        if( snapshot.hasData ) {
+
+          final peliculas = snapshot.data;
+          return ListView(
+            children: 
+              peliculas.map((pelicula) {
+                return ListTile(
+                  leading: FadeInImage(
+                    image: NetworkImage(pelicula.getPosterImg()),
+                    placeholder: AssetImage('assets/img/no-image.jpg'),
+                    width: 50.0,
+                    fit: BoxFit.cover,
+                  ),
+                  title: Text(pelicula.title),
+                  subtitle: Text(pelicula.originalTitle),
+                  onTap: () {
+                    close(context, null);
+                    pelicula.uniqueId = '';
+                    Navigator.pushNamed(context, 'detalle', arguments: pelicula);
+                  },
+                );
+              }).toList(),
+          );
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
   }
 }
