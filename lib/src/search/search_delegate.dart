a8c898c6894c2cc9b9c60bd2e381da18dde8378b
Bikcode <bikcodeh@gmail.com>
2020-04-29 21:45:44 -0500

    feature: implementa opcion de borrar y volver en el search delegate

--- lib/src/search/search_delegate.dart
+++ lib/src/search/search_delegate.dart
@@ -1,29 +1,42 @@
 import 'package:flutter/material.dart';
 
 class DataSearch extends SearchDelegate {
-  
   @override
   List<Widget> buildActions(BuildContext context) {
     // Son las acciones de nuestro appbar,como el de limpiar por ejemplo
-    return null;
+    return [
+      IconButton(
+        icon: Icon(Icons.clear), 
+        onPressed: () {
+          query = '';
+        }
+      )
+    ];
   }
 
   @override
   Widget buildLeading(BuildContext context) {
     // Icono a la izquierda del appbar, como el de la lupa
-    return null;
+    return IconButton(
+        icon: AnimatedIcon(
+          icon: AnimatedIcons.menu_arrow,
+          progress: transitionAnimation,
+        ),
+        onPressed: () {
+          close(context, null);
+        }
+      );
   }
 
   @override
   Widget buildResults(BuildContext context) {
     // crea los resultados que se van a mostrar
-    return null;
+    return Container();
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
     // las sugerencias que aparecen cuando la persona escribe
-    return null;
+    return Container();
   }
-
 }
